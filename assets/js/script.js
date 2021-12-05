f96e17254c0770a6569beca25e41c5773c9c3310
Lo Selby <laurenxselby@gmail.com>
2021-12-04 17:18:09 -0800

    added timeblock rows in HTML with past css class until functions are created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 
+
 // adds current date and time to the header
 function dateTime () {
     var timeNow = moment().format("MMMM Do, YYYY [at] hh:mm:ss a");
     $("#currentDay").text(timeNow);
+    return timeNow;
 }
-
+// sets interval of function dateTime to 1000 ms to update the date/time display every second
 setInterval(dateTime, 1000);
-// var dateTime = moment().format("MMM Do, YYYY [at] hh:mm:ss a");
-// $("#currentDay").text(dateTime);
+
+
+// updates the colors of each timeblock in the banner to either past due, current, or future colors
+// if (dateTime )
